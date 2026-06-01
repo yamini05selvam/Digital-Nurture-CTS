fcb7201629daf4a778aec75d28d76ac99836dd23
Yamini S <yaminiselvamyamini@gmail.com>
2026-06-01 12:24:38 +0530

    Add files via upload

---
+++ JAVA CODE/ArrayListExample.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListExample {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<String> students =
+                new ArrayList<>();
+
+        System.out.print(
+                "How many students? ");
+
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        for(int i = 0; i < n; i++) {
+
+            System.out.print("Enter name: ");
+
+            students.add(sc.nextLine());
+        }
+
+        System.out.println("Student List");
+
+        for(String name : students) {
+            System.out.println(name);
+        }
+    }
+}
