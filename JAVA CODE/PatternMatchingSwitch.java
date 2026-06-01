5cc35e888b837d37c925eb2a5157e19a8b7ca552
Yamini S <yaminiselvamyamini@gmail.com>
2026-06-01 12:29:01 +0530

    Add files via upload

---
+++ JAVA CODE/PatternMatchingSwitch.java
@@ -0,0 +1,33 @@
+public class PatternMatchingSwitch {
+
+    static void checkType(Object obj) {
+
+        switch(obj) {
+
+            case Integer i ->
+                    System.out.println(
+                            "Integer: " + i);
+
+            case String s ->
+                    System.out.println(
+                            "String: " + s);
+
+            case Double d ->
+                    System.out.println(
+                            "Double: " + d);
+
+            default ->
+                    System.out.println(
+                            "Unknown Type");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        checkType(100);
+
+        checkType("Hello");
+
+        checkType(99.5);
+    }
+}
